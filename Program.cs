4fd3ab981b0e871c2fd4bd3a58dc10e6b35ef8a7
Kristian Detlevsen <kbde63300@edu.ucl.dk>
2025-04-10 12:07:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CIDemo2_Team4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
